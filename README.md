4692c37ef5de837cabf32676097bdd3b11d4b0f3
Koutarouu <Kiritsugufel@gmail.com>
2018-05-19 14:34:13 -0500

    Second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# How to use CoffeeFa!
+
+Download and Execute in navigator the index.html or execute in the console un servidor de archivos estaticos como python -m http.server <PORT> .
---
+++ js/main.js
@@ -0,0 +1,98 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/sw.js")
+}
+
+;(function(){
+
+  let sticky = false
+  let currentPosition = 0
+
+  const imageCounter = $("[data-name='image-counter']").attr("content")
+  const email = "Kiritsugufel@gmail.com"
+
+  $("#contact-form").on("submit",function(ev){
+    ev.preventDefault()
+
+    sendForm($(this))
+
+    return false
+  })
+
+  $("#sticky-navigation").removeClass("hidden")
+  $("#sticky-navigation").slideUp(0)
+  checkScroll()
+
+  isOpen()
+
+  $("#menu-opener").on("click",toggleNav)
+
+  $(".menu-link").on("click",toggleNav)
+
+  setInterval(()=>{
+
+    if (currentPosition < imageCounter) {
+      currentPosition++
+    }else{
+      currentPosition = 0
+    }
+
+    $("#gallery .inner").css({
+      left: "-"+currentPosition*100+"%"
+    })
+
+  },4000)
+
+  $(window).scroll(checkScroll)
+
+  function checkScroll(){
+    const inBottom = isInBottom()
+
+    if (inBottom && !sticky) {
+      // Mostrar la navegacion
+      sticky = true
+      stickNavigation()
+    } else if(!inBottom && sticky){
+      // Ocultar la navegacion sticky
+      sticky = false
+      unStickNavigation()
+    }
+  }
+
+  function isOpen(){
+    // Reloj 24 => 5pm 11pm => 17 -- 23
+    let date = new Date()
+    const current_hour = date.getHours()
+
+    if (current_hour < 17 || current_hour > 21 ) {
+      console.log('Cerrado')
+      $("#is-open .text").html("Cerrado ahora <br> Abierto de 5:00pm a 11:00pm")
+    }
+  }
+
+  function toggleNav() {
+    $("#responsive-nav ul").toggleClass("active")
+    $("#menu-opener").toggleClass("glyphicon-menu-hamburger")
+  }
+
+  function stickNavigation(){
+    $("#description").addClass("fixed").removeClass("absolute")
+    $("#navigation").slideUp("fast")
+    $("#sticky-navigation").slideDown("fast")
+  }
+
+  function unStickNavigation(){
+    $("#description").removeClass("fixed").addClass("absolute")
+    $("#navigation").slideDown("fast")
+    $("#sticky-navigation").slideUp("fast")
+  }
+
+  function isInBottom(){
+    const $description = $("#description")
+    const descriptionHeight = $description.height()
+
+    return $(window).scrollTop() > $(window).height() - (descriptionHeight * 2)
+
+  }
+
+})()
+//{name:"coffe",image:"cf",pricing:"10.00"},{name:"cakes",image:"ck",pricing:"20.00"},{name:"cookie",image:"cook",pricing:"30.00"},{name:"maid",image:"md",pricing:"14.00"},{name:"christmasCake",image:"mxx",pricing:"60.00"},{name:"postre",image:"sp",pricing:"40.00"},{name:"chocoCake",image:"sj",pricing:"155.00"},{name:"naranjas",image:"nj",pricing:"134.00"}
